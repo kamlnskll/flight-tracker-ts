1f307f46f2234337fd4007587132508551a51fcb
kamlnskll <michael_kaminski@outlook.com>
2024-07-21 20:55:36 -0500

    save please!

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,20 +1,16 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 import { Map, Marker } from 'react-map-gl';
 import { useAircraftContext } from '../context/AircraftContext';
-import { Box } from '@mui/material';
-import { AircraftState, AircraftContextProps, AircraftProviderProps, IDebounce, IViewState, OSNAPIResponse } from '../types/AircraftTypes';
+import { IDebounce, OSNAPIResponse } from '../types/AircraftTypes';
 
+// Environmental variables declared at top level of the map.
 
-// Enviromental variables declared at top level of the map.
-
-const mapAccessToken = import.meta.env.VITE_MAPBOX_TOKEN
-const apiUrl = import.meta.env.VITE_OPENSKYNETWORK_API_URL
+const mapAccessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+const apiUrl = import.meta.env.VITE_OPENSKYNETWORK_API_URL;
 const cache: { [key: string]: any } = {};
 
-
-
-const debounce = ({func, wait}: IDebounce): (...args: any[]) => void => {
-  let timeout: NodeJS.Timeout
+const debounce = ({ func, wait }: IDebounce): (...args: any[]) => void => {
+  let timeout: NodeJS.Timeout;
   return (...args) => {
     clearTimeout(timeout);
     timeout = setTimeout(() => func.apply(this, args), wait);
@@ -30,120 +26,90 @@ const fetchWithCache = async (url: string): Promise<OSNAPIResponse> => {
   // If not in cache, fetch the data from the API
   const response = await fetch(url);
   const data: OSNAPIResponse = await response.json();
-  console.log(data, 'response from data')
-  
+  console.log(data, 'response from data');
+
   // Store the fetched data in the cache with the URL as the key
   cache[url] = data;
-  
+
   // Return the fetched data
   return data;
 };
 
 const MapPage: React.FC = () => {
+  const { aircraftData, updateAircraftData } = useAircraftContext();
 
-const [loading, setLoading] = useState(false);
-const [error, setError] = useState<null | string>(null);
-const {aircraftData, updateAircraftData } = useAircraftContext()
-const [viewState, setViewState] = useState<any>({
-  longitude: -100,
-  latitude: 40,
-  zoom: 3.5
-});
+  const fetchData = useCallback(() => {
+    fetchWithCache(apiUrl)
+      .then((res: any) => {
+        updateAircraftData(res);
+        console.log('Aircraft Data Console Log', aircraftData);
+      })
+      .catch((err) => console.error(err));
+  }, [apiUrl, updateAircraftData]);
 
-const onMove = (evt: any) => {
-  setViewState(evt.viewState)
-  // console.log(viewState.latitude, viewState.longitude)
-  console.log(aircraftData)
-}
+  useEffect(() => {
+    fetchData(); // Call immediately on page load
 
-const fetchData = useCallback(() => {
-  setLoading(true);
-  fetchWithCache(apiUrl)
-    .then((res: any) => {
-      console.log(res)
-      updateAircraftData(res);
-      setLoading(false);
-    })
-    .catch((err) => {
-      console.error(err) 
-      setError('Failed to fetch aircraft data')
-    setLoading(false)})
+    // const interval = setInterval(() => {
+    //   fetchData();
+    // }, 6000); // Call every 6 seconds
 
-}, [apiUrl, updateAircraftData]);
+    // return () => clearInterval(interval); // Clean up on unmount
+  }, [fetchData]);
 
-
-const filteredAircrafts = useMemo(() => {
-  if (!aircraftData || !aircraftData.states) {
-    return [];
-  }
-
-  return aircraftData?.states?.filter((aircraft: AircraftState[]) => {
-    const aircraftCategory = aircraft[16];
-    return typeof aircraftCategory === 'number' && aircraftCategory >= 2 && aircraftCategory <= 7 
-});
-}, [aircraftData]);
-
-
-
-
-useEffect(() => {
-  fetchData(); // Call immediately on page load
-  console.log(filteredAircrafts, 'filtered Aircrafts array')
-
-
-  // const interval = setInterval(() => {
-  //   fetchData();
-  // }, 20000); // Call every 6 seconds
-
-  // return () => clearInterval(interval); // Clean up on unmount
-}, [fetchData]);
-
-
-const aircraftMarkers = useMemo(() => {
-    // @ts-ignore
-    return filteredAircrafts?.states?.map((aircraft: any) => {
-      const {
-        icao24,
-        longitude,
-        latitude
-      } = aircraft;
-
-      if (typeof longitude === 'number' && typeof latitude === 'number') {
-        return (
-          <Marker
-            key={icao24}
-            longitude={longitude}
-            latitude={latitude}
-          >
-            <img src={'src/assets/airplane-svgrepo-com.svg'} width='24' alt="airplane marker" />
-          </Marker>
-        );
-      }
-      return null;
-    }).filter(Boolean);
-  }, [aircraftData]);
-
-
-return (
+  return (
     <>
-    <Box width="100vw" display="flex"
-          height="100vh" justifyContent="center"
-          alignItems="center">
-    <Map 
-    mapboxAccessToken={mapAccessToken}
-    {...viewState}
-    onMove={onMove}
-  style={{width: '100vw', height: '80vh'}}
-  mapStyle="mapbox://styles/mapbox/light-v11"
-  maxPitch={0}
-  minZoom={1}
-  maxZoom={12}
-    >
-  {aircraftMarkers}
-      </Map>   
-      </Box>
-    </>
-  )
-}
+      <Map
+        mapboxAccessToken={mapAccessToken}
+        initialViewState={{
+          longitude: -100.0,
+          latitude: 45,
+          zoom: 3,
+        }}
+        style={{ width: '100vh', height: '100vh' }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        maxPitch={0}
+        minZoom={1}
+        maxZoom={12}
+      >
+        {aircraftData?.states?.map((aircraft: any[], index: number) => {
+          const [
+            icao24,
+            callsign,
+            origin_country,
+            time_position,
+            last_contact,
+            longitude,
+            latitude,
+            baro_altitude,
+            on_ground,
+            velocity,
+            true_track,
+            vertical_rate,
+            sensors,
+            geo_altitude,
+            squawk,
+            spi,
+            position_source,
+            category
+          ] = aircraft;
 
-export default MapPage
+          // Ensure longitude and latitude are numbers
+          if (typeof longitude === 'number' && typeof latitude === 'number') {
+            return (
+              <Marker
+                key={index}
+                longitude={longitude}
+                latitude={latitude}
+              />
+            );
+          }
+
+          return null;
+        })}
+      </Map>
+    </>
+  );
+};
+
+export default MapPage;
--- src/types/AircraftTypes.ts
+++ src/types/AircraftTypes.ts
@@ -1,23 +1,23 @@
 export interface AircraftState {
-    icao24: string;
-    callsign: string | null;
-    origin_country: string;
-    time_position: number | null;
-    last_contact: number;
-    longitude: number 
-    latitude: number 
-    baro_altitude: number | null;
-    on_ground: boolean;
-    velocity: number | null;
-    true_track: number | null;
-    vertical_rate: number | null;
-    sensors: number[] | null;
-    geo_altitude: number | null;
-    squawk: string | null;
-    spi: boolean;
-    position_source: number;
-    category: number;
-  }
+  icao24: string;
+  callsign: string | null;
+  origin_country: string;
+  time_position: number | null;
+  last_contact: number;
+  longitude: number | null;
+  latitude: number | null;
+  baro_altitude: number | null;
+  on_ground: boolean;
+  velocity: number | null;
+  true_track: number | null;
+  vertical_rate: number | null;
+  sensors: number[] | null;
+  geo_altitude: number | null;
+  squawk: string | null;
+  spi: boolean;
+  position_source: number;
+  category: number;
+}
   
   export interface OSNAPIResponse {
     time: number,
