4f6a622fc2015c1c53613360730f1afa28d66d0e
Michael Kaminski <michael_kaminski@outlook.com>
2024-07-29 18:24:34 -0500

    ''

--- src/__tests__/AircraftDataCard.test.ts
+++ src/__tests__/AircraftDataCard.test.ts
@@ -1,5 +1,30 @@
-test('Get Aircraft Data From Props', () => {
+import { AircraftStateArray } from "../types/AircraftTypes";
+import AircraftDataCard from "../components/AircraftDataCard";
 
+const mockAircraftData: AircraftStateArray[] = [
+    [
+      'icao24',
+      'CALLSIGN',
+      'United States',
+      1623157000,
+      1623157000,
+      10.0,
+      20.0,
+      10000,
+      false,
+      500,
+      90,
+      0,
+      null,
+      11000,
+      'squawk',
+      false,
+      1,
+      1,
+    ],
+  ];
+
+test('Get Aircraft Data From Props', () => {
 
 })
 
